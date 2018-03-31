5ba2d8b9e439613068cd4aa52e5a55c8a5e2d36d
zede <zede@ventumit.ru>
2018-04-01 01:36:10 +0500

    add base of lib

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export interface IParseConfig {
+
+}
+
+export interface IStringifyConfig {
+
+}
+
+export function parse(data: string, params?: IParseConfig) {
+
+}
+
+export function stringify(data: any, params?: IStringifyConfig) {
+
+}
