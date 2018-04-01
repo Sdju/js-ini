05cb0080f26c3c518c37af22e721fb42e88fa719
zede <zede@ventumit.ru>
2018-04-01 21:39:11 +0500

    add tests for stringify

--- test/tests.ts
+++ test/tests.ts
@@ -22,6 +22,26 @@ v5 : who is who = who
 
 [test scope with spaces]
 mgm*1  : 2.5`;
+const ini3 = `v1=2
+v-2=true
+v 3=string
+
+[smbd]
+v5=who is who = who
+v2=what
+v1=5
+
+[test scope with spaces]
+mgm*1=2.5`;
+const ini4 = `v1: 2
+v-2: true
+v 3: string
+[smbd]
+v5: who is who = who
+v2: what
+v1: 5
+[test scope with spaces]
+mgm*1: 2.5`;
 const v1 = {
   v1: 2,
   ['v-2']: true,
@@ -57,4 +77,15 @@ test('ini parsing', () => {
   expect(parse(ini2, { comment: '#', delimiter: ':' })).toEqual(v1);
 
   expect(parse(ini2, { comment: '#', delimiter: ':', autoTyping: false })).toEqual(v2);
+});
+
+test('ini stringify', () => {
+  expect(stringify(v1)).toBe(ini3);
+
+  expect(stringify(v1, {
+    blankLine: false,
+    delimiter: ':',
+    spaceAfter: true,
+    spaceBefore: false,
+  })).toBe(ini4);
 });
