92f57e2a11cea80ecaf74c71586fe9fd896beb87
denis.ch <denis.ch@koshelek.ru>
2023-02-12 00:20:13 +0700

    move from jest to vitest

--- test/tools.test.ts
+++ test/tools.test.ts
@@ -1,3 +1,6 @@
+import {
+  describe, it, expect, beforeAll, afterAll, afterEach,
+} from 'vitest';
 import { join } from 'path';
 import { promises as fs } from 'fs';
 import { readIniFile } from '../src/tools/read-ini-file';
@@ -41,41 +44,42 @@ const v1 = {
 
 const basePath = join(__dirname, 'playground');
 const iniPath = join(basePath, 'test.ini');
-
-beforeAll(async () => {
-  await fs.mkdir(basePath);
-});
-
-afterAll(async () => {
-  await fs.rmdir(basePath);
-});
-
-afterEach(async () => {
-  const files = await fs.readdir(basePath);
-  const promises = files.map((file: string) => fs.unlink(join(basePath, file)));
-  return Promise.all(promises);
-});
-
-test('read-ini-file', async () => {
-  await fs.writeFile(iniPath, ini1, 'utf-8');
-
-  const iniObj = await readIniFile(iniPath, {
-    comment: '#',
-    delimiter: ':',
+describe('base js-ini test', () => {
+  beforeAll(async () => {
+    await fs.mkdir(basePath);
   });
 
-  expect(iniObj).toEqual(v1);
-});
-
-test('write-ini-file', async () => {
-  await writeIniFile(iniPath, v1, {
-    blankLine: false,
-    delimiter: ':',
-    spaceAfter: true,
-    spaceBefore: false,
+  afterAll(async () => {
+    await fs.rmdir(basePath);
   });
 
-  const data = await fs.readFile(iniPath, 'utf-8');
+  afterEach(async () => {
+    const files = await fs.readdir(basePath);
+    const promises = files.map((file: string) => fs.unlink(join(basePath, file)));
+    await Promise.all(promises);
+  });
 
-  expect(data).toBe(ini2);
+  it('read-ini-file', async () => {
+    await fs.writeFile(iniPath, ini1, 'utf-8');
+
+    const iniObj = await readIniFile(iniPath, {
+      comment: '#',
+      delimiter: ':',
+    });
+
+    expect(iniObj).toEqual(v1);
+  });
+
+  it('write-ini-file', async () => {
+    await writeIniFile(iniPath, v1, {
+      blankLine: false,
+      delimiter: ':',
+      spaceAfter: true,
+      spaceBefore: false,
+    });
+
+    const data = await fs.readFile(iniPath, 'utf-8');
+
+    expect(data).toBe(ini2);
+  });
 });
